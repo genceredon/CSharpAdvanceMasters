b7cd069eabfc241de7e8d07a3a0fa7bb29997594
933620 <933620@SVPHLP00389>
2023-01-20 16:52:53 +0800

    Add project files.

---
+++ Session_08/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Session_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
